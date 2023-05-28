9f8c4c168a32ffa2e3595a8fa2c0c2f13bbffb74
hyggge <hyggge6@gmail.com>
2023-05-28 22:30:27 +0800

    禁用vditor工具栏的快捷键

--- src/ts/util/editorCommonEvent.ts
+++ src/ts/util/editorCommonEvent.ts
@@ -213,31 +213,32 @@ export const hotkeyEvent = (vditor: IVditor, editorElement: HTMLElement) => {
             return true;
         }
 
+
         // toolbar action
-        vditor.options.toolbar.find((menuItem: IMenuItem) => {
-            if (!menuItem.hotkey || menuItem.toolbar) {
-                if (menuItem.toolbar) {
-                    const sub = menuItem.toolbar.find((subMenuItem: IMenuItem) => {
-                        if (!subMenuItem.hotkey) {
-                            return false;
-                        }
-                        if (matchHotKey(subMenuItem.hotkey, event)) {
-                            vditor.toolbar.elements[subMenuItem.name].children[0]
-                                .dispatchEvent(new CustomEvent(getEventName()));
-                            event.preventDefault();
-                            return true;
-                        }
-                    });
-                    return sub ? true : false;
-                }
-                return false;
-            }
-            if (matchHotKey(menuItem.hotkey, event)) {
-                vditor.toolbar.elements[menuItem.name].children[0].dispatchEvent(new CustomEvent(getEventName()));
-                event.preventDefault();
-                return true;
-            }
-        });
+        // vditor.options.toolbar.find((menuItem: IMenuItem) => {
+        //     if (!menuItem.hotkey || menuItem.toolbar) {
+        //         if (menuItem.toolbar) {
+        //             const sub = menuItem.toolbar.find((subMenuItem: IMenuItem) => {
+        //                 if (!subMenuItem.hotkey) {
+        //                     return false;
+        //                 }
+        //                 if (matchHotKey(subMenuItem.hotkey, event)) {
+        //                     vditor.toolbar.elements[subMenuItem.name].children[0]
+        //                         .dispatchEvent(new CustomEvent(getEventName()));
+        //                     event.preventDefault();
+        //                     return true;
+        //                 }
+        //             });
+        //             return sub ? true : false;
+        //         }
+        //         return false;
+        //     }
+        //     if (matchHotKey(menuItem.hotkey, event)) {
+        //         vditor.toolbar.elements[menuItem.name].children[0].dispatchEvent(new CustomEvent(getEventName()));
+        //         event.preventDefault();
+        //         return true;
+        //     }
+        // });
     });
 };
 
